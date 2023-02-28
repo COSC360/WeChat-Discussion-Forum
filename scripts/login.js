b001748f5c659fdcf1bf2707f68dce661d13cd2d
aryanagn <aryanbhat87@gmail.com>
2023-02-27 20:37:46 -0800

    createAcct-login pages and created folders for files

---
+++ scripts/login.js
@@ -0,0 +1,9 @@
+function LoginPage() {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("create-account").style.display = "none";
+}
+
+function CreateAccountPage() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("create-account").style.display = "block";
+}
