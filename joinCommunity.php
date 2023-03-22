f7b1534d396537539346a8556d2023877cc0edbd
jossw24 <94143372+jossw24@users.noreply.github.com>
2023-03-21 17:37:22 -0700

    Users can now join communities. If they are already in community, they will not be added to the user_community table again

--- joinCommunity.php
+++ joinCommunity.php
@@ -10,13 +10,18 @@ $user_id = $_SESSION['user_id'];
 // Get the community ID from the submitted form data
 $community_id = $_POST['community_id'];
 
-// Insert a new row into the user_community table
-$query = "INSERT INTO user_community (user_id, community_id) VALUES ('$user_id', '$community_id')";
-mysqli_query($conn, $query);
+$query = "SELECT * FROM user_community WHERE user_id = '$user_id' AND community_id = '$community_id'";
+$result = mysqli_query($conn,$query);
+if(mysqli_num_rows($result) == 0){
+    // Insert a new row into the user_community table
+
+    $query = "INSERT INTO user_community (user_id, community_id) VALUES ('$user_id', '$community_id')";
+    mysqli_query($conn, $query);
+}
 
 // Close the database connection
 mysqli_close($conn);
 
-header("Location: community.php");
+header("Location: home.php");
 }
 ?>
